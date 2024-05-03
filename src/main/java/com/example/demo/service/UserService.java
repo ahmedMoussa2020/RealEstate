2175c8912e4038a90f13a3601e1fe5fbcef5acd6
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2024-05-03 14:54:21 -0400

    Refactored the UserService class

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,38 +1,39 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-// This activity aims to implement a UserService class that contains business logic and calls UserDao to access the data stored in our User database table.
 
 import org.springframework.stereotype.Service;
-
 import com.example.demo.jdbc.UserBean;
 import com.example.demo.jdbc.UserDao;
+import com.example.demo.jpa.User;
+import com.example.demo.repository.UserRepository;
 
+//This activity aims to implement a UserService class that contains business logic and calls UserDao to access the data stored in our User database table.
 @Service
 public class UserService {
 
 	// The @Autowired annotation tells Spring to inject a UserDao instance into the
 	// userDao variable:
-	@Autowired
-	// instance variable called userDao
-	UserDao userDao;
+	@Autowired // // instance variable called userDao
+	UserRepository userRepository;
 
 	// listUsers() method to get all the existing Users in the database:
-	public List<UserBean> listUsers() {
+	public List<User> listUsers() {
 
-		return this.userDao.listUsers();
+		return this.userRepository.findAll();
 	}
 
 	// findByUsername() method to get a user by username from the database:
-	public UserBean findByUsername(String username) {
+	public Optional<User> findByUsername(String username) {
 
-		return this.userDao.findByUsername(username);
+		return this.userRepository.findByUsername(username);
 	}
 
-	public void createUser(UserBean user) {
-		this.userDao.createUser(user);
+	public void createUser(User user) {
+		this.userRepository.save(user);
 	}
 }
