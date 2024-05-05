9791e719c1c9e13ef97b944faa3e4e88df5668c1
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2024-05-04 20:31:02 -0400

    create FeedMetaDataRepository interface

---
+++ src/main/java/com/example/demo/repository/FeedMetaDataRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.jpa.FeedMetaData;
+
+
+// JpaRepository is a Spring Data interface that extends the CrudRepository interface and provides additional methods for working with JPA entities.
+// { }: The body for the interface is empty since no additional methods are defined.
+// Overall, the FeedMetaDataRepository interface extends JpaRepository and provides basic CRUD operations for the FeedMetaData entity class.
+public interface FeedMetaDataRepository extends JpaRepository<FeedMetaData, Integer> {
+	
+}
