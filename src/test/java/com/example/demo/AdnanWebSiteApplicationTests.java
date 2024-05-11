4e919f2562abec322c8951d8dd5bf75293d6e475
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2024-05-11 17:03:19 -0400

    modify the Spring Boot test class,

--- src/test/java/com/example/demo/AdnanWebSiteApplicationTests.java
+++ src/test/java/com/example/demo/AdnanWebSiteApplicationTests.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.DisplayName;
+import org.h2.tools.Server;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+
+
 
 @SpringBootTest
-class AdnanWebSiteApplicationTests {
+@ActiveProfiles("test")
+class FeedAppApplicationTests {
 
+	@DisplayName("Demo Test")
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@MockBean
+	Server server;
 }
