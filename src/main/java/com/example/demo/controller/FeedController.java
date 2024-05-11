d71fe736bfb0f65fe446d14c95a1cd7501433ce2
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2024-05-10 23:48:03 -0400

    Define feedController

---
+++ src/main/java/com/example/demo/controller/FeedController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.jpa.Feed;
+import com.example.demo.service.FeedService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/feeds")
+public class FeedController {
+
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	FeedService feedService;
+
+	@PostMapping
+	public Feed createFeed(@RequestBody Feed feed) {
+
+		logger.debug("Creating Feed");
+
+		return this.feedService.createFeed(feed);
+	}
+}
